1f56c4f6a2172f47e37e2a1c49418e3c8e94978a
Ajin <fibreun@gmail.com>
2017-08-24 19:58:39 +0300

    up commit

--- waxom/js/common.js
+++ waxom/js/common.js
@@ -1,1 +1,31 @@
-console.log('hello!');
+var time = 4,
+	cc = 1;
+$(window).scroll(function(){
+	$('.counter').each(function(){
+		var cPos = $(this).offset().top,
+		topWindow = $(window).scrollTop();
+
+		if(cPos < topWindow + 800) {
+			if(cc < 2){
+				$('.number').addClass('viz');
+				$('.counter div').each(function(){
+				var i = 1,
+				    num = $(this).data('num'),
+				    step = 1000 * time / num,
+				    that = $(this),
+				    int = setInterval(function(){
+				      if (i <= num) {
+				        that.html(i);
+				      }
+				      else {
+				      	cc = cc + 2;
+				        clearInterval(int);
+				      }
+				      i++;
+				    },step);
+				});
+			}
+		}
+	});
+});
+
