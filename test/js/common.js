fdc597e63d082489b49fc6f6da7dd8031d75e04f
Ajin <fibreun@gmail.com>
2018-01-09 14:46:20 +0300

    commit up

---
+++ test/js/common.js
@@ -0,0 +1,25 @@
+$(function(){
+
+	$("#wr-tabs").on("click", ".tab", function(){
+
+		var tabs = $("#wr-tabs .tab"),
+		    cont = $("#wr-tabs .tab-cont");
+
+		// Удаляем классы active
+		tabs.removeClass("active");
+		cont.removeClass("active");
+		// Добавляем классы active
+		$(this).addClass("active");
+		cont.eq($(this).index()).addClass("active");
+
+		return false;
+	});
+
+	$('.nav-toggle').on('click', function(){
+  		$('#nav-menu').toggleClass('active-menu');
+	});
+
+	$('.description').on('click', function(){
+  		$(this).toggleClass('active-description');
+	});
+});
